894a8195f7f3d30dc09b4ab0c113e7bea0e242d5
rahulrank <rahulrank.rank0@gmail.com>
2015-07-05 14:10:17 +0530

    first upload
    new upload

---
+++ resources/views/app.blade.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Laravel</title>
+
+	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+
+	<!-- Fonts -->
+	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+	<style type="text/css">
+
+	.navbar-nav > li {
+		padding-left: 30px;
+	}
+
+	.navbar-nav > li:hover {
+		font-size: 17px;
+	}
+    
+    .title {
+                font-size: 25px;
+                font-family: 'Lato';
+                font-weight: 300;
+    }
+
+    .quote {
+    	font-size: 18px;
+    	font-family: sans-serif;
+    }
+
+    .logo-header {
+		background-color: #6e0023;
+    }
+
+    #logo {
+    	box-align: center;
+    }
+
+	</style>
+</head>
+<body>
+	<div class="container-fluid logo-header">
+		<img class="image-responsive" id="logo" src="{{ asset('/images/anj media.png') }}" alt="logo" width="200" height="100">
+	</div>
+	<nav class="navbar navbar-default">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					<span class="sr-only">Toggle Navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#"><span class="glyphicon glyphicon-home"></span></a>
+			</div>
+
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav">
+					<li><a href="{{ url('/') }}">Home</a></li>
+					<li><a href="#">Advertiser</a></li>
+					<li><a href="#">Contact Us</a></li>
+				</ul>
+
+				<ul class="nav navbar-nav navbar-right">
+					@if (Auth::guest())
+						<li><a href="{{ url('/auth/login') }}">Login</a></li>
+						<li><a href="{{ url('/auth/register') }}">Register</a></li>
+					@else
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+							<ul class="dropdown-menu" role="menu">
+								<li><a href="{{ url('/auth/logout') }}">Logout</a></li>
+							</ul>
+						</li>
+					@endif
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+	@yield('content')
+
+	<!-- Scripts -->
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
